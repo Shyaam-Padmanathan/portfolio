972330cc328fd485713461f13eb50282a232856c
Shyaam <shyaam.p99@gmail.com>
2024-04-28 23:09:46 +0530

    Activated the about and projects

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -1,17 +1,14 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
-import { useTranslation } from "react-i18next";
 
 function Type() {
-  const { t } = useTranslation();
-
   return (
     <Typewriter
       options={{
         strings: [
-          t('Typer1'),
-          t('Typer2'),
-          t('Typer3')
+          'Full Stack Developer',
+          'Software Engineer',
+          'Passionate about Science and Engineering'  
         ],
         autoStart: true,
         loop: true,
